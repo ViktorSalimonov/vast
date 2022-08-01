ab387e20aa1e5e24193a07d8d7750c06fb363b45
Viktor Salimonov <viktor.salimonov@outlook.com>
2022-08-01 16:41:31 +0400

    Add a function to process video file and get its duration

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module vast
 
 go 1.18
+
+require gopkg.in/vansante/go-ffprobe.v2 v2.1.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gopkg.in/vansante/go-ffprobe.v2"
 )
 
+func get_video_duration(filename string) int {
+	// return video duration in seconds
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	fileReader, err := os.Open(filename)
+	if err != nil {
+		log.Panicf("Error opening test file: %v", err)
+	}
+
+	data, err := ffprobe.ProbeReader(ctx, fileReader)
+	if err != nil {
+		log.Panicf("Error getting data: %v", err)
+	}
+	return int(data.Format.Duration().Seconds())
+}
+
 func main() {
-	fmt.Println("VAST service")
+	FILENAME := "./videos/video_1.mp4"
+
+	duration := get_video_duration(FILENAME)
+	fmt.Println(duration)
 }
